4787278d420c99e610f74d88a66bd72fceacd631
Ricky-Patel <ricky.patel.git@gmail.com>
2024-07-22 15:05:28 -0400

    added more clangd to LSP and path, setup a dashboard with alpha, and got auto completion set up

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,5 @@ vim.cmd("set shiftwidth=2")
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.g.mapleader = " "
+vim.cmd("set textwidth=80")
+vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', {})
